0762ab639b8dfb84cdb0bef504b87f92e319ce5e
cosmos-explorer <lian.yang.work@gmail.com>
2022-04-20 16:31:13 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,8 +2,7 @@ import { queryParams } from "./utils";
 
 export const userName = '随机用户';// + Math.round(Math.random() * 1000);
 
-const secret = queryParams['secret']; //'YmE3LWNkN2EtNCUyMDIxMTEyNDEzNDYxOV9fNV9fMTI1X18yNjVfX2RlZmF1bHRfMmY1MTE=';
-
+const secret = queryParams['secret'];
 const configBase = {
     options: {
         secret,
